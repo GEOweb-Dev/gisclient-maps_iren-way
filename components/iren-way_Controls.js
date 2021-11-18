81e241fce4acd329f8fa2cc3f2bd7eb73bd796c4
Marco Giraudi (Old Snapo) <mgiraudi@soluzioninrete.it>
2021-11-18 16:01:12 +0100

    Initial commit

---
+++ components/iren-way_Controls.js
@@ -0,0 +1,311 @@
+// *******************************************************************************************
+//********************************************************************************************
+//**** Custom Controls for GEOweb - Iren Way integration
+
+window.GCComponents["Layers"].addLayer('layer-iren-way-highlight', {
+    displayInLayerSwitcher:false,
+    styleMap: new OpenLayers.StyleMap({
+        'default': {
+            fill: false,
+            fillColor: "red",
+            fillOpacity: 0.9,
+            hoverFillColor: "white",
+            hoverFillOpacity: 0.9,
+            strokeColor: "red",
+            strokeOpacity: 0.9,
+            strokeWidth: 10,
+            strokeLinecap: "round",
+            strokeDashstyle: "solid",
+            hoverStrokeColor: "red",
+            hoverStrokeOpacity: 1,
+            hoverStrokeWidth: 10,
+            pointRadius: 8,
+            hoverPointRadius: 1,
+            hoverPointUnit: "%",
+            pointerEvents: "visiblePainted",
+            cursor: "inherit"
+        },
+        'select': {
+            fill: true,
+            fillColor: "red",
+            fillOpacity: 0.9,
+            hoverFillColor: "white",
+            hoverFillOpacity: 0.9,
+            strokeColor: "red",
+            strokeOpacity: 1,
+            strokeWidth: 10,
+            strokeLinecap: "round",
+            strokeDashstyle: "solid",
+            hoverStrokeColor: "red",
+            hoverStrokeOpacity: 1,
+            hoverStrokeWidth: 10,
+            pointRadius: 8,
+            hoverPointRadius: 1,
+            hoverPointUnit: "%",
+            pointerEvents: "visiblePainted",
+            cursor: "pointer"
+        },
+        'temporary': {
+            fill: true,
+            fillColor: "EEA652",
+            fillOpacity: 0.2,
+            hoverFillColor: "white",
+            hoverFillOpacity: 0.8,
+            strokeColor: "#EEA652",
+            strokeOpacity: 1,
+            strokeLinecap: "round",
+            strokeWidth: 4,
+            strokeDashstyle: "solid",
+            hoverStrokeColor: "red",
+            hoverStrokeOpacity: 1,
+            hoverStrokeWidth: 0.2,
+            pointRadius: 6,
+            hoverPointRadius: 1,
+            hoverPointUnit: "%",
+            pointerEvents: "visiblePainted",
+            cursor: "pointer"
+        }
+    })
+}, {
+    "sketchcomplete": function(obj) {
+
+    },
+    "featureadded": function(obj) {
+        var feature = obj.feature;
+        var irenwayArrayData = [];
+
+        for (var j=0; j<clientConfig.IRENWAY_LAYERS.length; j++) {
+            if (clientConfig.IRENWAY_LAYERS[j].layers.indexOf(feature.featureTypeName) > -1) {
+                for (var k = 0; k < clientConfig.IRENWAY_LAYERS[j].fields.length; k++) {
+                    var dataField = clientConfig.IRENWAY_LAYERS[j].outvars[k];
+                    var dataValue = feature.attributes[clientConfig.IRENWAY_LAYERS[j].fields[k]];
+                    if (clientConfig.IRENWAY_LAYERS[j].numfeats > 1) {
+                        var tmpObj = {};
+                        tmpObj[dataField] = dataValue;
+                        irenwayArrayData.push(tmpObj);
+                    }
+                    else {
+                        this.irenwayExportData[dataField] = dataValue;
+                    }
+                }
+                var outItemName = clientConfig.IRENWAY_LAYERS[j].outitem;
+                if (typeof(outItemName) != 'undefined') {
+                    this.irenwayExportData['irenway_outitem'] = outItemName;
+                    if (irenwayArrayData.length > 0) {
+                        this.irenwayExportData[outItemName] = irenwayArrayData;
+                    }
+                }
+            }
+        }
+    }
+});
+
+
+// **** Point marker layer (TODO: style)
+window.GCComponents["Layers"].addLayer('layer-iren-way-markpoint', {
+    displayInLayerSwitcher:false,
+    styleMap: new OpenLayers.StyleMap({
+        'default': {
+            cursor: "inherit",
+            graphicHeight: 32,
+            externalGraphic: "../../plugins/gisclient-maps_iren-way/css/images/irenway_marker.png"
+        }
+    })
+}, {
+    "sketchcomplete": function(obj) {
+        var tmpGeom = obj.feature.geometry.clone();
+        var srid = this.map.displayProjection?this.map.displayProjection:this.map.projection;
+        if (srid != this.map.projection) {
+            tmpGeom.transform(this.map.projection, srid);
+        }
+        window.GCComponents.Functions.sendToIrenWay({x: tmpGeom.x, y:tmpGeom.y, srid:srid});
+
+        if (typeof(clientConfig.IRENWAY_SRID) !== 'undefined') {
+            tmpGeom = obj.feature.geometry.clone();
+            srid = clientConfig.IRENWAY_SRID;
+            if (srid != this.map.projection) {
+                tmpGeom.transform(this.map.projection, srid);
+            }
+            window.GCComponents.Functions.sendToIrenWay({x: tmpGeom.x, y:tmpGeom.y, srid:srid});
+        }
+
+        this.removeAllFeatures();
+    },
+    "featureadded": function(obj) {
+        // **** Get main selection control
+        var selectControls = this.map.getControlsBy('gc_id', 'control-querytoolbar');
+        if (selectControls.length != 1)
+            return;
+        if (!selectControls[0].controls)
+            return;
+        var selectControl = selectControls[0];
+        selectControl.controls[0].layers = [];
+        // **** insert configured WFS layers
+        if (typeof(clientConfig.IRENWAY_LAYERS) === 'undefined') {
+            return;
+        }
+        var resultLayer = this.map.getLayersByName('layer-iren-way-highlight')[0];
+        if (resultLayer) {
+            resultLayer.irenwayExportData = {};
+        }
+        else {
+            return;
+        }
+        var featureTypes = '';
+        var selectLayers = [];
+        var numFeatures = 0;
+        var selectControlAuto = this.map.getControlsBy('gc_id', 'control-iren-way-autoselect')[0];
+
+        for (var i=0; i<clientConfig.IRENWAY_LAYERS.length; i++) {
+            for (var j=0; j<clientConfig.IRENWAY_LAYERS[i].layers.length; j++) {
+                var tmpLayer = selectControl.getLayerFromFeature(clientConfig.IRENWAY_LAYERS[i].layers[j]);
+                var idx;
+                for (idx = 0; idx < selectLayers.length; idx++)  {
+                    if (selectLayers[idx].id === tmpLayer.id)
+                        break;
+                }
+                if (idx === selectLayers.length)
+                    selectLayers.push(tmpLayer);
+                featureTypes += clientConfig.IRENWAY_LAYERS[i].layers[j] + ',';
+            }
+            numFeatures += clientConfig.IRENWAY_LAYERS[i].numfeats;
+        }
+        selectControlAuto.layers = selectLayers;
+        selectControlAuto.queryFeatureType = featureTypes.substring(0, featureTypes.length -1);
+        selectControlAuto.wfsCache = selectControls[0].wfsCache;
+        selectControlAuto.resultLayer = resultLayer;
+        selectControlAuto.maxFeatures = numFeatures;
+        selectControlAuto.activate();
+        selectControlAuto.select(obj.feature.geometry);
+        selectControlAuto.deactivate();
+
+        return true;
+    }
+});
+
+// **** Auto select click control
+window.GCComponents["Controls"].addControl('control-iren-way-autoselect', function(map){
+    return new OpenLayers.Control.QueryMap(
+        OpenLayers.Handler.Click,
+        {
+            gc_id: 'control-iren-way-autoselect',
+            baseUrl: GisClientMap.baseUrl,
+            maxFeatures:1,
+            deactivateAfterSelect: true,
+            vectorFeaturesOverLimit: new Array(),
+            eventListeners: {
+                'activate': function(){
+                    var selectControls = this.map.getControlsBy('gc_id', 'control-querytoolbar');
+                    if (selectControls.length != 1)
+                        return false;
+
+                },
+                'endQueryMap': function(event) {
+                    if(event.layer.irenwayExportData) {
+                        var irenWayExportData = event.layer.irenwayExportData;
+                        var exportToIrenWay = {};
+                        $.each(clientConfig.IRENWAY_OUTDATA, function (key, item) {
+                            var arrOutData = [];
+                            for (var i=0; i<item.length; i++) {
+                                var outDataOption = item[i];
+                                for (var j=0; j<outDataOption.length; j++) {
+                                    var outDataVar = outDataOption[j];
+                                    if (outDataVar.hasOwnProperty('const')) {
+                                        arrOutData.push(outDataVar.const);
+                                    }
+                                    else if (outDataVar.hasOwnProperty('var')) {
+                                        if (irenWayExportData.hasOwnProperty(outDataVar.var)) {
+                                            arrOutData.push(irenWayExportData[outDataVar.var]);
+                                        }
+                                        else {
+                                            arrOutData = [];
+                                            break;
+                                        }
+                                    }
+                                    else {
+                                        arrOutData = [];
+                                        break;
+                                    }
+                                }
+                                if (arrOutData.length > 0) {
+                                    break;
+                                }
+                            }
+                            exportToIrenWay[key] = arrOutData.join(clientConfig.IRENWAY_CONCATSTRING);
+                        });
+                        window.GCComponents.Functions.sendToIrenWay(exportToIrenWay);
+                        event.layer.irenwayExportData = {};
+                    }
+                    if (this.resultLayer.hasOwnProperty('renderQueue')) {
+                        delete this.resultLayer.renderQueue;
+                    }
+                },
+            }
+        }
+    )
+});
+
+// **** Point marker draw control
+window.GCComponents["Controls"].addControl('control-iren-way-markpoint', function(map){
+    return new OpenLayers.Control.DrawFeature(
+        map.getLayersByName('layer-iren-way-markpoint')[0],
+        OpenLayers.Handler.Point,
+        {
+            gc_id: 'control-iren-way-markpoint',
+            eventListeners: {
+                'activate': function(e){
+                    if (map.currentControl != this) {
+                        map.currentControl.deactivate();
+                        var touchControl = map.getControlsByClass("OpenLayers.Control.TouchNavigation");
+                        if (touchControl.length > 0) {
+                            touchControl[0].dragPan.deactivate();
+                        }
+                    }
+                    map.currentControl=this;
+                },
+                'deactivate': function(e){
+                    var touchControl = map.getControlsByClass("OpenLayers.Control.TouchNavigation");
+                    if (touchControl.length > 0) {
+                        touchControl[0].dragPan.activate();
+                    }
+
+                    var btnControl = map.getControlsBy('id', 'button-iren-way-markpoint')[0];
+                    if (btnControl.active)
+                        btnControl.deactivate();
+
+                }
+            }
+        }
+    )
+});
+
+// **** Toolbar button
+window.GCComponents["SideToolbar.Buttons"].addButton (
+    'button-iren-way-markpoint',
+    'Esporta coordinate per IRENWAY',
+    'glyphicon-white glyphicon-tag',
+    function() {
+        if (sidebarPanel.handleEvent || typeof(sidebarPanel.handleEvent) === 'undefined')
+        {
+            this.map.getLayersByName('layer-iren-way-highlight')[0].removeAllFeatures();
+            this.map.getLayersByName('layer-iren-way-markpoint')[0].removeAllFeatures();
+            window.GCComponents.Functions.resetIrenWayData();
+            if (this.active) {
+                this.deactivate();
+                var drawControl = this.map.getControlsBy('gc_id', 'control-iren-way-markpoint');
+                if (drawControl.length == 1)
+                    drawControl[0].deactivate();
+            }
+            else
+            {
+                this.activate();
+                var drawControl = this.map.getControlsBy('gc_id', 'control-iren-way-markpoint');
+                if (drawControl.length == 1)
+                    drawControl[0].activate();
+            }
+            if (typeof(sidebarPanel.handleEvent) !== 'undefined')
+                sidebarPanel.handleEvent = false;
+        }
+    },
+    {button_group: 'tools'}
+);
---
+++ components/iren-way_Functions.js
@@ -0,0 +1,181 @@
+// *******************************************************************************************
+//*********************************************************************************************************
+//**** Functions for GEOweb - Iren Way integration
+
+window.GCComponents.Functions.centerMap = function (xCoord, yCoord, srid, zoom) {
+    var lonLat = new OpenLayers.LonLat(xCoord, yCoord);
+    var GCMap = GisClientMap.map;
+    var retValue = {result: 'ok'};
+    if (srid != GCMap.projection) {
+        lonLat.transform(srid, GCMap.projection);
+    }
+    if(!GCMap.isValidLonLat(lonLat)){
+        retValue.result = 'error';
+        retValue.message = 'Posizione non valida: X ' +lonLat.lon+', Y '+lonLat.lat+', SRID ' + srid;
+        alert(retValue.message);
+        return retValue;
+    }
+    if(!GCMap.getMaxExtent().containsLonLat(lonLat)){
+        retValue.result = 'error';
+        retValue.message = 'Posizione fuori extent: X ' +lonLat.lon+', Y '+lonLat.lat+', SRID ' + srid;
+        alert(retValue.message);
+        return retValue;
+    }
+    GCMap.setCenter(lonLat);
+    GCMap.zoomToScale(zoom, true);
+    return retValue;
+}
+
+window.GCComponents.Functions.centerMapOnFeature = function(layer, whereCond, values) {
+    var retValue = {result: 'ok'};
+    var fType = GisClientMap.getFeatureType(layer);
+
+    if(!fType) {
+        retValue.result = 'error';
+        retValue.message = 'Errore: il featureType '+selectedFeatureType+' non esiste';
+        return retValue;
+    }
+
+    var params = {
+        featureType: layer,
+        query: whereCond,
+        values: values
+    };
+    params.projectName = GisClientMap.projectName;
+    params.mapsetName = GisClientMap.mapsetName;
+    params.srid = GisClientMap.map.projection;
+
+    $.ajax({
+        url: clientConfig.GISCLIENT_URL + '/services/xMapQuery.php',
+        method: 'POST',
+        dataType: 'json',
+        data: params,
+        success: function(response) {
+            if(!response || typeof(response) != 'object') {
+                retValue.result = 'error';
+                retValue.message = 'Errore di sistema';
+                return retValue;
+            }
+            if(!response.length) {
+                retValue.result = 'error';
+                retValue.message = 'Nessun risultato';
+                return retValue;
+            }
+
+            var geometries = new OpenLayers.Geometry.Collection(),
+                len = response.length, result, i, geometry, feature;
+
+            for(i = 0; i < len; i++) {
+                result = response[i];
+
+                geometry = result.gc_geom && OpenLayers.Geometry.fromWKT(result.gc_geom);
+                if(!geometry) continue;
+                delete result.gc_geom;
+
+                geometries.addComponent(geometry);
+            }
+
+            if (geometries.components.length < 1) {
+                retValue.result = 'error';
+                retValue.message = 'Nessuna geometria nel risultato della ricerca';
+                return retValue;
+            }
+
+            geometries.calculateBounds();
+            GisClientMap.map.zoomToExtent(geometries.bounds);
+
+            return retValue;
+        },
+        error: function() {
+            retValue.result = 'error';
+            retValue.message = 'Errore di sistema';
+            return retValue;
+        }
+    });
+}
+
+window.GCComponents.Functions.parseQueryString = function() {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    var urlParams = {};
+    while (match = search.exec(query))
+        urlParams[decode(match[1])] = decode(match[2]);
+
+    return urlParams;
+}
+
+window.GCComponents.Functions.updateQueryString = function(updateItems) {
+    var queryStringItems = window.GCComponents.Functions.parseQueryString();
+    for (var item in updateItems) {
+        queryStringItems[item] = updateItems[item];
+    };
+
+    if (history.pushState) {
+        var newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?';
+        for (var item in queryStringItems) {
+            newUrl += item + '=' + queryStringItems[item] + '&';
+        };
+        window.history.pushState({path:newUrl},'',newUrl);
+    }
+}
+
+window.GCComponents.InitFunctions.centerMapIrenWay = function () {
+    var queryStringItems = window.GCComponents.Functions.parseQueryString();
+    if(queryStringItems.x && queryStringItems.y) {
+        var zoom = typeof (queryStringItems.zoom) == 'undefined'?clientConfig.IRENWAY_DEFAULT_ZOOM:queryStringItems.zoom;
+        var sridDefault = typeof(GisClientMap.map.displayProjection) == 'undefined'?GisClientMap.map.projection:GisClientMap.map.displayProjection;
+        var srid = typeof (queryStringItems.srid) == 'undefined'?sridDefault:queryStringItems.srid;
+        window.GCComponents.Functions.centerMap(queryStringItems.x, queryStringItems.y, srid, zoom)
+    }
+    else if (queryStringItems.layer) {
+        var fType = GisClientMap.getFeatureType(queryStringItems.layer);
+        if (!fType)
+            return;
+
+        var values = {};
+        var queryString = '';
+        for (var i=0; i<fType.properties.length; i++) {
+            if (fType.properties[i].searchType == 0)
+                continue;
+            var fieldName = fType.properties[i].name;
+            var valPlaceholder = 'param' + i;
+            if (typeof (queryStringItems[fieldName]) != 'undefined') {
+                if (queryString.length == 0) {
+                    queryString += fieldName + ' = :' + valPlaceholder;
+                }
+                else {
+                    queryString += ' AND ' + fieldName + ' = :' + valPlaceholder;
+                }
+                values[valPlaceholder] = queryStringItems[fieldName];
+            }
+        }
+        if (queryString.length > 0) {
+            queryString = '(' + queryString + ')';
+            window.GCComponents.Functions.centerMapOnFeature(queryStringItems.layer, queryString, values);
+        }
+    }
+}
+
+window.GCComponents.Functions.sendToIrenWay = function(irenwayItems) {
+    window.GCComponents.Functions.updateQueryString(irenwayItems);
+}
+
+window.GCComponents.Functions.resetIrenWayData = function(){
+    if (history.pushState) {
+        var newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        newUrl += '?mapset=' + GisClientMap.map.config.mapsetName;
+        window.history.pushState({path:newUrl},'',newUrl);
+    }
+}
+
+window.GCComponents.InitFunctions.initIrenWayPlugin = function(map) {
+    var selectControls = map.getControlsBy('gc_id', 'control-querytoolbar');
+    if (selectControls.length == 1)
+        selectControls[0].events.register('endQueryMap', null, function(e) {
+            this.irenwaySelection = false;
+        });
+}
